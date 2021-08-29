d73911668295d64ae9b807e41748d84f3ebc20fe
Vazgen Gasparyan <89462790+gasparyan2005@users.noreply.github.com>
2021-08-29 17:41:07 +0400

    I added try / except to avoid ValueError in case of incorrect typing.

--- password.py
+++ password.py
@@ -2,15 +2,13 @@
 from random import sample
 from string import ascii_lowercase as al, ascii_uppercase as au
 
-lens = int(input("\nWhat is the length of the password: "))
-
-for pas in range(0 , 5):
-  password=''.join(sample('_*&!?;:$%^-/.,1234567890'+au+al,lens))
-  print('password:  ', password)
-
-    
-
-# code by Vazgen Gasparyan
-# Facebook: https://bit.ly/3zhk3PO
-
-# Thank you!
+for i in range(5):
+    while True:
+        length = input('\nEnter 8-32 for length password -  ')
+        try:
+            while int(length)<8 or int(length)>32:
+                length = input('\nInvalid length for password\nPlease enter 8-32 for length password -  ')
+        except ValueError: continue
+        if int(length)>=8 or int(length)<=32:
+            print('password: ', ''.join(sample('_*&!?;:$%^-/.,1234567890'+au+al,int(length))))
+            break
